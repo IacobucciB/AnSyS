391d27812791208e422fe0edc9b71771489d2b1a
Iacobucci Juan B. <link00222@gmail.com>
2024-06-18 10:14:58 -0300

    Create ej6a.m

---
+++ tp1/ej6a.m
@@ -0,0 +1,9 @@
+t = [-2:.001:8];
+
+function h = tri1(t)
+    % TRI1 señal del ejercicio 2 de la Practica 1
+    h = (t+1).*(t >= -1 & t < 1) + (-t+3).*(t >= 1 & t < 3);
+end
+
+h = tri1(2*t-3);
+plot(t,h)
