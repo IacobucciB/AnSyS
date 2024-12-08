35b3a737b99af1b02d07b17f3870cdee06c4ba1e
Iacobucci Juan B. <link00222@gmail.com>
2024-12-08 17:55:58 -0300

    Update ej_dos_3.m

--- pu1/IPS_2024_PU1_m/ejercicio_dos/ej_dos_3.m
+++ pu1/IPS_2024_PU1_m/ejercicio_dos/ej_dos_3.m
@@ -1,34 +1,34 @@
 clear all;
 close all;
 clc;
-addpath('../funciones')
+addpath('../funciones');
 
 % Cargar el archivo de audio
 [x, fs] = audioread('audio.wav');
 
-% Aplicar el sistema a la señal
+% Aplicar el sistema a la senal
 y = sys_canal(x);
 
-% Escuchar las señales
-disp('Reproduciendo la señal de entrada...');
+% Escuchar las senales
+disp('Reproduciendo la senal de entrada...');
 sound(x, fs);
 pause(length(x)/fs + 1);
 
-disp('Reproduciendo la señal de salida...');
+disp('Reproduciendo la senal de salida...');
 sound(y, fs);
 
-% Graficar las señales
+% Graficar las senales
 t = (0:length(x)-1) / fs;
 figure;
 subplot(2,1,1);
 plot(t, x);
-title('Señal de entrada');
+title('Senal de entrada');
 xlabel('Tiempo (s)');
 ylabel('Amplitud');
 
 subplot(2,1,2);
 plot(t, y);
-title('Señal de salida');
+title('Senal de salida');
 xlabel('Tiempo (s)');
 ylabel('Amplitud');
 
